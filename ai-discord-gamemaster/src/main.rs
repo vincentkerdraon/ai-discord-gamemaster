2c6f0cf0e9c42b0b1e8346a21d338a1e379e1b2e
Vincent KERDRAON <vincent.kerdraon@gmail.com>
2024-12-02 17:36:52 -0700

    connect discord client
    /spent 80m

--- ai-discord-gamemaster/src/main.rs
+++ ai-discord-gamemaster/src/main.rs
@@ -1,21 +1,58 @@
 use axum::{response::Html, routing::get, Router};
-use tracing::info;
+use serenity::{async_trait, model::gateway::Ready, prelude::*, Client};
+use std::env;
+use tracing::{debug, info};
 
-async fn hello_world() -> Html<&'static str> {
-    Html("Hello, World!")
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
 }
 
 #[tokio::main]
 async fn main() {
+    // Initialize the tracing subscriber
     tracing_subscriber::fmt::init();
-    info!("Starting server...");
 
-    // build our application with a route
-    let app = Router::new().route("/", get(hello_world));
+    // Configure the client with your Discord bot token
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    // run it
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service()) // Convert the router into a make service
+    // Create a new instance of the Client, logging in as a bot. The builder method
+    // returns an error here if the token is invalid or other problems with the
+    // bot are present.
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    info!("Create client discord...");
+    info!(token);
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
         .await
-        .unwrap();
+        .expect("Err creating client");
+
+    let _axum_handle = tokio::spawn(async move {
+        info!("Starting server...");
+
+        // build our application with a route
+        let app = Router::new().route("/", get(hello_world));
+
+        // run it
+        axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    });
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
+
+async fn hello_world() -> Html<&'static str> {
+    Html("Hello, World!")
 }
